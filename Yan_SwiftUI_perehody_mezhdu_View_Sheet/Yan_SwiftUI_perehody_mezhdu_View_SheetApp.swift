b257d9357469f3092be16964372b2817a1ea94a3
Kir Proto Q <kir.proto@ya.ru>
2022-02-10 11:21:58 +0500

    Initial Commit

---
+++ Yan_SwiftUI_perehody_mezhdu_View_Sheet/Yan_SwiftUI_perehody_mezhdu_View_SheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_perehody_mezhdu_View_SheetApp.swift
+//  Yan_SwiftUI_perehody_mezhdu_View_Sheet
+//
+//  Created by KIR Q on 2022.02.10.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_perehody_mezhdu_View_SheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
